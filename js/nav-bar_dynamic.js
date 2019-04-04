adb5322819a484ffe1368173a62a5edd38c25f20
doncasanova <ethanjuan@yahoo.com>
2019-04-03 20:53:35 -0500

    test

--- js/nav-bar_dynamic.js
+++ js/nav-bar_dynamic.js
@@ -72,7 +72,7 @@ function addNavBar() {
 
 function addHeader() {
     $(".loadHeader").prepend(`  <div class="col-5 col-md-3 text-right">
-                <img class=" mr-2 logoImage" src="./img/images/logo.png" alt="DR Logo">
+                <img class=" mr-2 logoImage" src="./img/images/logo.jpg" alt="DR Logo">
             </div>
             <div class=" col-7 col-md-9 page-header-title row triangle">
                 <div class="col-8 col-md-9">
